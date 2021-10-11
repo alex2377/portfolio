c1ef64d67505aceae583f284e17dc6a0d930ce3a
Aleksei <23leha77@mail.ru>
2021-10-11 20:49:52 +0300

    start

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "dist"
+            baseDir: "dist" 
         }
     });
 
@@ -23,12 +23,17 @@ gulp.task('styles', function() {
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))//for import in git
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 })
 gulp.task('html',function(){
     return gulp.src("src/*.html")
@@ -38,21 +43,26 @@ gulp.task('html',function(){
 gulp.task('scripts',function(){
     return gulp.src("src/js/**/*.js")
         .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 })
 gulp.task('fonts',function(){
     return gulp.src("src/fonts/**/*")
         .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
 })
 gulp.task('icons',function(){
     return gulp.src("src/icons/**/*")
         .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
 })
 gulp.task('mailer',function(){
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest("dist/mailer"))
+        .pipe(browserSync.stream());
 })
 gulp.task('images',function(){
     return gulp.src("src/img/**/*")
         .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
 })
 gulp.task('default', gulp.parallel('watch', 'server', 'styles','html','scripts','fonts','icons','mailer','images'));
