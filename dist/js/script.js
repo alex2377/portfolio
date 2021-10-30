7b708ff1f4a0b667098320bac44043169216f26d
Aleksei <23leha77@mail.ru>
2021-10-30 15:09:35 +0300

    finished_fourth_section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,16 @@
 const hamburger=document.querySelector('.hamburger');
 const menu=document.querySelector('.menu');
 const closeElem=document.querySelector('.menu__close');
+const ratingItems=document.querySelectorAll('.skills__ratingsItem');
 hamburger.addEventListener('click',()=>{
     menu.classList.add('active');
 }
     )
 closeElem.addEventListener('click',()=>{
     menu.classList.remove('active');
+})
+window.addEventListener('load',function(){
+ratingItems.forEach(elem=>{
+    elem.querySelector('.skills__ratingsProgressFilling').style.width=elem.querySelector('.skills__ratingsPercent').textContent;
+})
 })
